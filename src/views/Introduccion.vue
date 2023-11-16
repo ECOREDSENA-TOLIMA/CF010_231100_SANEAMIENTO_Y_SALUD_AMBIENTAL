7bd9eeeac4bb4137e0150e3b53088e9c7fbb1d25
veimarcm <vcelis@sena.edu.co>
2023-11-15 21:07:07 -0500

    CFA MATERIAL FUENTES
    SE CARGA CFA, MATERIAL Y FUENTES PARA EMPAQUETADO

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -15,8 +15,8 @@
     .row.justify-content-center.align-items-center.md-5   
       .col-lg-12(data-aos="fade-left")     
         figure
-          .video          
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/AXEO6d-BgXg" title="Contaminación atmosférica y cambio climático" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen)
 </template>
 
 <script>
